c44c12a01a3a89a31c689931ecf24a5d761d427b
Temple Dees <temple.dees@gmail.com>
2025-08-10 00:47:53 -0700

    typewriter effect, navbar working, projects started

--- myPortfolio/src/App.tsx
+++ myPortfolio/src/App.tsx
@@ -3,17 +3,32 @@ import NavBar from './components/NavBar'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import ResumeButton from './components/ResumeButton'
 
 
 const App = () => {
   return (
     <div className="gradient-background">
       <div className="content">
-        App
-        <NavBar />
-        <About />
-        <Projects />
-        <Contact />
+        <div>
+          <NavBar />
+          <ResumeButton />
+        </div>
+        <div>
+          <section id="about" className="scroll-mt-32 scroll-smooth">
+            <About />
+          </section>
+        </div>
+        <div>
+          <section id="projects" className="scroll-mt-32 scroll-smooth">
+            <Projects />
+          </section>
+        </div>
+        <div>
+          <section id="contact" className="scroll-mt-32 scroll-smooth">
+            <Contact />
+          </section>
+        </div>
         </div>
     </div>
   )
--- myPortfolio/src/components/NavBar.tsx
+++ myPortfolio/src/components/NavBar.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import { HashLink } from 'react-router-hash-link'
 
 const NavBar = () => {
   return (
-    <div>
-       <div className="bg-white rounded-full w-96 mt-6 h-16 gap-16 fixed left-1/2 transform -translate-x-1/2 flex items-center justify-center opacity-50 drop-shadow-xl">
+    <div className="flex justify-center pt-8">
+       <div className="bg-white rounded-full w-96 h-16 gap-16 flex items-center justify-center opacity-70 drop-shadow-xl outline-2 border-black content-center">
             <div>
-                <HashLink smooth to="/#about" className="text-black text-lg font-semibold">About</HashLink>
+                <a href="#about" className="text-black text-lg font-semibold bg-sky-700:hover">About</a>
             </div>
             <div>
-                <HashLink smooth to="/#projects" className="text-black text-lg font-semibold">Projects</HashLink>
+                <a href="#projects" className="text-black text-lg font-semibold bg-sky-700:hover">Projects</a>
             </div>
             <div>
-                <HashLink smooth to="/#contact" className="text-black text-lg font-semibold">Contact</HashLink>
+                <a href="#contact" className="text-black text-lg font-semibold bg-sky-700:hover">Contact</a>
             </div>
         </div>  
     </div> 
