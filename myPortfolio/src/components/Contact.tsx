a48b14e1302877f3f84d376dc69aa2c7bf5ce573
Temple Dees <temple.dees@gmail.com>
2025-08-10 20:59:39 -0700

    fixed navbar position, projects cards, contact form + submission, linkedin link

--- myPortfolio/src/components/Contact.tsx
+++ myPortfolio/src/components/Contact.tsx
@@ -1,16 +1,54 @@
 import React from 'react'
+import Swal from 'sweetalert2'
 
 const Contact = () => {
+    function handleClick() {
+        window.open('https://www.linkedin.com/in/temple-dees/'), '_blank'
+    }
+    const [result, setResult] = React.useState("");
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.currentTarget);
+
+    formData.append("access_key", "32c91746-de71-40e8-88f4-e25e5c9d389c");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      (event.target as HTMLFormElement).reset();
+      Swal.fire({
+        title: "Success!",
+        text: "Message sent successfully!",
+        icon: "success"
+        });
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
-    <div className="h-screen">
+    <div className="h-screen mt-32">
         <div className="flex justify-center">
             <h2 className="text-6xl font-semibold drop-shadow-lg">Contact</h2>
         </div>
         <div className="flex justify-center mt-8">
-            <p className="text-2xl">Feel free to reach out via email or connect with me on LinkedIn!</p>
+            <p className="text-2xl">Feel free to reach out via email or connect with me on </p>
+            <p onClick={handleClick} className="text-blue-500 hover:underline ml-2 text-2xl">LinkedIn</p>
         </div>
-        <div className="flex justify-center mt-4">
-            <a href="https://www.linkedin.com/in/templedees/" className="text-blue-500 hover:underline ml-4">LinkedIn</a>
+        <div className="mt-16">
+            <form onSubmit={onSubmit} className="flex flex-col items-center mt-8 gap-4">
+                <input type="text" placeholder="Your Name" className="border-2 border-gray-300 rounded-xl p-3 mb-4 w-1/3 bg-white shadow-xl/20 opacity-75" name="name" required />
+                <input type="email" placeholder="Your Email" className="border-2 border-gray-300 rounded-xl p-3 mb-4 w-1/3 bg-white shadow-xl/20 opacity-75" name="email" required/>
+                <textarea placeholder="Your Message" name="message" className="border-2 border-gray-300 rounded-xl p-3 mb-4 w-1/3 h-32 bg-white shadow-xl/20 opacity-75" required></textarea>
+                <button type="submit" className="bg-white text-black rounded-xl p-2 w-1/3 hover:bg-fuschia-600 shadow-xl/20 opacity-75">Send Message</button>
+            </form>
         </div>
     </div>
   )
--- myPortfolio/src/components/Projects.tsx
+++ myPortfolio/src/components/Projects.tsx
@@ -6,15 +6,94 @@ const Projects = () => {
         <div className="flex justify-center">
             <h2 className="text-6xl font-semibold drop-shadow-lg">Projects</h2>
         </div>
-        <div className="mt-8 flex flex-col items-center gap-8">
-            <div className="border-solid p-[30px] border-2">
-                <h4>Project 1</h4>
+        <div className="mt-8 flex flex-row items-center gap-4 mt-32 justify-center ml-6 mr-6 ">
+            <div className="border-solid pl-[20px] pr-[20px] pb-[15px] pt-[15px] border-2 bg-white shadow-xl/20 drop-shadow-xl rounded-3xl">
+                <div>
+                    <img src="src/assets/plantgpt-inlab.jpg" className="rounded-lg w-fit"></img>
+                </div>
+                <div>
+                    <h3 className="text-4xl">PlantGPT</h3>
+                    <p className="text-md">Explores conversational user interfaces applied to plant care, providing personalized plant
+care recommendations based on health data and voices to users.</p>
+                </div>
+                <div className="flex grid-rows-4 gap-4 mt-8">
+                    <div className="rounded-4xl bg-gray-300 border-2 w-[80px] h-[30px] text-center">Python</div>
+                    <div className="rounded-4xl bg-gray-300 border-2 w-[150px] h-[30px] text-center">LLama 3.1 Model</div>
+                    <div className="rounded-4xl bg-gray-300 border-2 w-[80px] h-[30px] text-center" >Arduino</div>
+                    <div className="rounded-4xl bg-gray-300 border-2 w-[80px] h-[30px] text-center">Sensors</div>
+                </div>
+                <div className="flex items-center mt-2 gap-24 ml-8 mb-2">
+                    <div className="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                        </svg>
+                        <a href="https://github.com/tdees01/plant-gpt" className="text-blue-500 hover:underline">Source Code</a>
+                    </div>
+                    <div className="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                        </svg>
+                        <a href="https://drive.google.com/file/d/1qsCxRKyD-LGE8upmpEHl69D74tfElpia/view?usp=sharing" className="text-blue-500 hover:underline">View Demo</a>
+                    </div>
+                </div>
             </div>
-            <div className="border-solid p-[30px] border-2">
-                <h4>Project 2</h4>
+            <div className="border-solid pl-[20px] pr-[20px] pb-[15px] pt-[15px] border-2 bg-white shadow-xl/20 drop-shadow-xl rounded-3xl">
+                <div>
+                    <img src="src/assets/plantgpt-inlab.jpg" className="rounded-lg w-fit"></img>
+                </div>
+                <div>
+                    <h3 className="text-4xl">Project 1</h3>
+                    <p className="text-md">A brief description of Project 1.</p>
+                </div>
+                <div className="flex grid-rows-4 gap-4 mt-8">
+                    <div className="rounded-4xl bg-gray-300 border-2 w-[80px] h-[30px] text-center">Python</div>
+                    <div className="rounded-4xl bg-gray-300 border-2 w-[150px] h-[30px] text-center">LLama 3.1 Model</div>
+                    <div className="rounded-4xl bg-gray-300 border-2 w-[80px] h-[30px] text-center" >Arduino</div>
+                    <div className="rounded-4xl bg-gray-300 border-2 w-[80px] h-[30px] text-center">Sensors</div>
+                </div>
+                <div className="flex items-center mt-8 gap-24 ml-8 mb-4">
+                    <div className="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                        </svg>
+                        <a href="https://github.com/tdees01/plant-gpt" className="text-blue-500 hover:underline">Source Code</a>
+                    </div>
+                    <div className="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                        </svg>
+                        <a href="https://drive.google.com/file/d/1qsCxRKyD-LGE8upmpEHl69D74tfElpia/view?usp=sharing" className="text-blue-500 hover:underline">View Demo</a>
+                    </div>
+                </div>
             </div>
-            <div className="border-solid p-[30px] border-2">
-                <h4>Project 3</h4>
+            <div className="border-solid pl-[20px] pr-[20px] pb-[15px] pt-[15px] border-2 bg-white shadow-xl/20 drop-shadow-xl rounded-3xl">
+                <div>
+                    <img src="src/assets/plantgpt-inlab.jpg" className="rounded-lg w-fit"></img>
+                </div>
+                <div>
+                    <h3 className="text-4xl">Project 1</h3>
+                    <p className="text-md">A brief description of Project 1.</p>
+                </div>
+                <div className="flex grid-rows-4 gap-4 mt-8">
+                    <div className="rounded-4xl bg-gray-300 border-2 w-[80px] h-[30px] text-center">Python</div>
+                    <div className="rounded-4xl bg-gray-300 border-2 w-[150px] h-[30px] text-center">LLama 3.1 Model</div>
+                    <div className="rounded-4xl bg-gray-300 border-2 w-[80px] h-[30px] text-center" >Arduino</div>
+                    <div className="rounded-4xl bg-gray-300 border-2 w-[80px] h-[30px] text-center">Sensors</div>
+                </div>
+                <div className="flex items-center mt-8 gap-24 ml-8 mb-4">
+                    <div className="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                        </svg>
+                        <a href="https://github.com/tdees01/plant-gpt" className="text-blue-500 hover:underline">Source Code</a>
+                    </div>
+                    <div className="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                        </svg>
+                        <a href="https://drive.google.com/file/d/1qsCxRKyD-LGE8upmpEHl69D74tfElpia/view?usp=sharing" className="text-blue-500 hover:underline">View Demo</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
