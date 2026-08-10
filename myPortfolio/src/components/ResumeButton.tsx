cab4708a1b1b618cbe24992dba4760c59d0d6a59
Temple Dees <156852797+tdees01@users.noreply.github.com>
2026-08-10 01:25:10 -0400

    Update Resume, change classification

--- myPortfolio/src/components/ResumeButton.tsx
+++ myPortfolio/src/components/ResumeButton.tsx
@@ -1,6 +1,6 @@
 export default function ResumeButton() {
     function handleClick() {
-        window.open('https://drive.google.com/file/d/1OsUXC9BsDsZT67ML5w86BGSadwwadyae/view?usp=sharing'), '_blank'
+        window.open('https://drive.google.com/file/d/1hrP6p7-hYsArq2Ptu41xK-IchjJrU3cF/view?usp=sharing'), '_blank'
     }
     return (
         <div className={"fixed top-8 right-4 z-50"}>
